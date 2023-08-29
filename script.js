3f41e78d95e2a7507c3e5337513b17d3c7f23bcc
Pavan Navde <136722639+pavannavde@users.noreply.github.com>
2023-08-29 11:35:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+  let out = document.getElementById("output");
+   let result= new Promise((resolve)=>{
+	   setTimeout(()=>{
+		   resolve("Hello, world!");
+	   },1000)
+   })
+result.then((data)=>{
+	out.innerText=data;
+})
